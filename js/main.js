e1b119f852e96aab6f320daf430fd6b9adbdf37d
yramocan <yramocan@umich.edu>
2016-03-24 18:10:32 -0400

    initialize repo

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#video-wrapper').tubular({
+    videoId: 'SdbJNg2dsng',
+    playerVars: {
+      iv_load_policy: 3,
+      disablekb: 1,
+      modestbranding: 1,
+      autoplay: 1,
+      controls: 0,
+      showinfo: 0,
+      wmode: 'transparent',
+      branding: 0,
+      rel: 0,
+      autohide: 1,
+      origin: window.location.origin
+    }
+  });
+});
